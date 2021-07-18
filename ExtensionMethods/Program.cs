260f1af6b8a4f8bd39d634ae714fcb9c071a31ac
rbeninche <rbeninche@gmail.com>
2021-07-17 21:19:23 -0400

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+            person.FirstName = "What is your first name: ".RequestString();
+            person.Lastname = "What is your last name: ".RequestString();
+            person.Age = "What is your age: ".RequestInt(0, 120);
+
+            Console.ReadLine();
+        }
+    }
+}
